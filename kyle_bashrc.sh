ab8a7256ac38660003ddb40fb7905383cb715853
kyle <kyle0923@qq.com>
2025-02-17 15:23:15 -0500

    Update kyle_bashrc.sh PS1

--- kyle_bashrc.sh
+++ kyle_bashrc.sh
@@ -59,7 +59,7 @@ handle_long_ps1() {
 }
 
 if [ ! -e '/.dockerenv' ]; then
-    export PS1="\$(handle_long_ps1 \"\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\")"
+    export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}$(handle_long_ps1 "\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[33m\]$(parse_git_branch)\[\033[00m\]")'
 else
     export PS1="\$(handle_long_ps1 \"\[\e[44m\]\u@\h\[\e[0m\] \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\")"
 fi
