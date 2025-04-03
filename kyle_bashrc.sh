bedc13f871e11a0ec5b147f32d143acbb2eac899
kyle <kyle0923@qq.com>
2025-04-03 03:17:05 -0400

    Update kyle_bashrc.sh

--- kyle_bashrc.sh
+++ kyle_bashrc.sh
@@ -45,6 +45,15 @@ parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
+parse_git_branch_with_arrow() {
+    local git_branch=$(git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')
+    if [[ -n "$git_branch" ]]; then
+        builtin echo -e "\001\e[43;32m\002\001\xEE\x82\002\xB0\001\e[43;30m\002 $git_branch \001\e[0;33m\002\001\xEE\x82\002\xB0\001\e[0m\002"
+    else
+        builtin echo -e "\001\e[0;32m\002\001\xEE\x82\002\xB0\001\e[0m\002"
+    fi
+}
+
 # when PS1 is too long, prompt starts in next line
 handle_long_ps1() {
     local input=$(echo $1 | sed -e "s,$HOME,~,")
@@ -61,7 +70,8 @@ handle_long_ps1() {
 
 if [ ! -e '/.dockerenv' ]; then
     # export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}$(handle_long_ps1 "\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[33m\]$(parse_git_branch)\[\033[00m\]")'
-    export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}$(handle_long_ps1 "\[\E[44m\E[30m\]\u@\h\[\E[42m\E[34m\]\[\356\202\]\260\[\E[30m\]\w\[\E[43m\E[32m\]\[\356\202\]\260\[\E[43m\E[30m\]$(parse_git_branch)\[\E[0m\E[33m\]\[\356\202\]\260\[\E[0m\]")'
+    # export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}$(handle_long_ps1 "\[\E[44m\E[30m\]\u@\h\[\E[42m\E[34m\]\[\356\202\]\260\[\E[30m\]\w\[\E[43m\E[32m\]\[\356\202\]\260\[\E[43m\E[30m\]$(parse_git_branch)\[\E[0m\E[33m\]\[\356\202\]\260\[\E[0m\]")'
+    export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}$(handle_long_ps1 "\[\E[44m\E[30m\]\u@\h \[\E[42m\E[34m\]\[\356\202\]\260\[\E[30m\] \w $(parse_git_branch_with_arrow)\[\E[0m\]")'
 else
     export PS1="\$(handle_long_ps1 \"\[\e[44m\]\u@\h\[\e[0m\] \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\")"
 fi
